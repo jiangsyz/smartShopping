418c81a027fea9b439473dea53c0f21358de7169
jiangsyz <305460883@qq.com>
2018-02-15 23:07:12 +0800

    member实现person

--- backend/models/member/member.php
+++ backend/models/member/member.php
@@ -5,9 +5,15 @@ use Yii;
 use yii\base\SmartException;
 use yii\db\SmartActiveRecord;
 use backend\models\model\source;
+use backend\models\model\person;
 //========================================
-class member extends source{
+class member extends source implements person{
 	//返回资源类型
 	public function getSourceType(){return source::TYPE_MEMBER;}
 	//========================================
+	//获取昵称
+	public function getNickName(){return $this->nickName;}
+	//========================================
+	//获取头像
+	public function getAvatar(){return $this->avatar;}
 }
