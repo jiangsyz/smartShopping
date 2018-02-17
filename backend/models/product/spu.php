93ecd71c66c1ab9dbedbc42fe0a8ab0c7b20cb4a
jiangsyz <305460883@qq.com>
2018-02-17 09:12:51 +0800

    spu

---
+++ backend/models/product/spu.php
@@ -0,0 +1,10 @@
+<?php
+//商品
+namespace backend\models\product;
+use Yii;
+use yii\base\SmartException;
+use yii\db\SmartActiveRecord;
+//========================================
+class spu extends SmartActiveRecord{
+
+}
