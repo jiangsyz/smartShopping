5dc7b962c81da823f90b8cd8d474ec11a23b0c08
jiangsyz <305460883@qq.com>
2018-02-17 21:04:41 +0800

    skuMemberPrice

---
+++ backend/models/product/skuMemberPrice.php
@@ -0,0 +1,8 @@
+<?php
+//库存计量单元的会员价
+namespace backend\models\product;
+use Yii;
+use yii\base\SmartException;
+use yii\db\SmartActiveRecord;
+//========================================
+class skuMemberPrice extends SmartActiveRecord{}
