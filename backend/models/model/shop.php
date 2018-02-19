9e3acd2eb50fc96cb1d0fb888c244b77c97bce01
jiangsyz <305460883@qq.com>
2018-02-19 21:44:14 +0800

    shop

--- backend/models/model/shop.php
+++ backend/models/model/shop.php
@@ -3,9 +3,4 @@
 namespace backend\models\model;
 use Yii;
 //========================================
-interface shop extends person{
-	//获取昵称
-	public function getNickName();
-	//获取头像
-	public function getAvatar();
-}
+interface shop extends person{}
