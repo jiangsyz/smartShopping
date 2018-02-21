68b0728f46f9a004c692e1e4357978be8a2b6eb2
jiangsyz <305460883@qq.com>
2018-02-21 10:43:02 +0800

    订单申请单元修改

--- backend/models/model/orderApplicant.php
+++ backend/models/model/orderApplicant.php
@@ -6,13 +6,13 @@ use Yii;
 interface orderApplicant{
 	//申请方类型
 	const TYPE_SHOPPING_CART=1;//购物车
-	const TYPE_FAST_BUYING;//快速购买
-	//========================================
-	//获取结算单位下的所有购买行为
-	public function getBuyingRecords();
+	const TYPE_FAST_BUYING=2;//快速购买
 	//========================================
 	//获取申请方类型
 	public function getOrderApplicantType();
+	//========================================
+	//获取结算单位下的所有购买行为
+	public function getBuyingRecords();
 	//========================================
 	//获取会员
 	public function getMember();
