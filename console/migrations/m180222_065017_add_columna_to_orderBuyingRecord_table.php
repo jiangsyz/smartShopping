d7661b243119ef19cbc7229ba9e55a4648943f33
jiangsyz <305460883@qq.com>
2018-02-28 12:07:15 +0800

    rm console/migrations/m180222_065017_add_columna_to_orderBuyingRecord_table.php 文件名错了

--- console/migrations/m180222_065017_add_columna_to_orderBuyingRecord_table.php
+++
@@ -1,8 +0,0 @@
-<?php
-use yii\db\Migration;
-class m180222_065017_add_columna_to_orderBuyingRecord_table extends Migration{
-    public function up(){
-        $this->addColumn('order_buying_record','price','DOUBLE NOT NULL COMMENT "单价" AFTER `buyingCount`');
-        $this->addColumn('order_buying_record','finalPrice','DOUBLE NOT NULL COMMENT "成交单价" AFTER `price`');
-    }
-}
