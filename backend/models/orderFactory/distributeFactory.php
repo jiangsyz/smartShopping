48cfbb0e54399a63f252604e4d0ae6c03e26dacd
jiangsyz <305460883@qq.com>
2018-03-06 18:51:07 +0800

    add backend/models/orderFactory/mainOrderFactory.php/backend/models/orderFactory/distributeFactory.php

---
+++ backend/models/orderFactory/distributeFactory.php
@@ -0,0 +1,71 @@
+<?php
+//基于配送方式分单的订单工厂
+namespace backend\models\orderFactory;
+use Yii;
+use yii\base\SmartException;
+use yii\base\Component;
+use backend\models\distribute\distribute;
+use backend\models\member\member;
+//========================================
+class distributeFactory extends orderFactory{
+	//商家
+	public $distributeType=NULL;
+	//购买行为集合
+	private $buyingRecords=array();
+	//========================================
+	//向工厂添加购买行为
+	public function addBuyingRecord(buyingRecord $buyingRecord){
+		//在购买行为集合中,销售单元唯一编号是索引,一个销售单元的购买行为只能有一条
+		$index=$buyingRecord->salesUnit->getSourceNo();
+		if(isset($this->buyingRecords[$index])) throw new SmartException("bRecord {$index} existed");
+		//加入销售单元和数量
+		$this->buyingRecords[$index]=$buyingRecord;
+	}
+	//========================================
+	//获取价格(不含运费)
+	public function getPrice(){
+		$price=0;
+		//累加选中的购买行为的价格
+		foreach($this->buyingRecords as $r) if($r->isSelected) $price+=$r->getPrice();
+		return $price;
+	}
+	//========================================
+	//获取最终成交价格(不含运费)
+	public function getFinalPrice(){
+		$finalPrice=0;
+		//累加选中的购买行为的价格
+		foreach($this->buyingRecords as $r) if($r->isSelected) $finalPrice+=$r->getFinalPrice();
+		return $finalPrice;
+	}
+	//========================================
+	//获取运费
+	public function getFreight(){return NULL;}
+	//========================================
+	//获取订单标题
+	public function getTitle(){return $this->distributeType;}
+	//========================================
+	//获取索引,在整个订单树中唯一
+	public function getIndex(){return md5($this->distributeType);}
+	//========================================
+	//获取订单中购物行为的总重量
+	public function getWeight(){
+		$total=0;
+		foreach($this->buyingRecords as $buyingRecord){
+			//没选中的不计算
+			if(!$buyingRecord->isSelected) continue;
+			//获取重量
+			$weight=$buyingRecord->salesUnit->getProperty("weight");
+			//计入总计
+			if($weight) $total+=$weight*$buyingRecord->buyCount;
+		}
+	}
+	//========================================
+	//初始化订单
+	public function initOrder(member $member){
+		parent::initOrder($member);
+		//加入购物行为
+		foreach($this->buyingRecords as $buyingRecord) 
+			if($buyingRecord->isSelected) $this->order->addBuyingRecord($buyingRecord);
+		return $this->order;
+	}
+}
---
+++ backend/models/orderFactory/mainOrderFactory.php
@@ -0,0 +1,67 @@
+<?php
+//主订单工厂
+namespace backend\models\orderFactory;
+use Yii;
+use yii\base\SmartException;
+use yii\base\Component;
+use backend\models\model\shop;
+use backend\models\member\member;
+use backend\models\distribute\distribute;
+//========================================
+class mainOrderFactory extends orderFactory{
+	//向工厂添加购买行为
+	public function addBuyingRecord(buyingRecord $buyingRecord){
+		//获取配送方式
+		$distributeType=$buyingRecord->salesUnit->getDistributeType();
+		//获取子订单工厂
+		if(!isset($this->childFactories[$distributeType])){
+			$distributeFactoryData=array();
+			$distributeFactoryData['member']=$this->member;
+			$distributeFactoryData['parentFactory']=$this;
+			$distributeFactoryData['distributeType']=$distributeType;
+			$this->childFactories[$distributeType]=new distributeFactory($distributeFactoryData);
+		}
+		//将购物行为加入子订单工厂
+		$this->childFactories[$distributeType]->addBuyingRecord($buyingRecord);
+	}
+	//========================================
+	//获取价格(不含运费)
+	public function getPrice(){
+		$price=0;
+		//累加子订单的价格
+		foreach($this->childFactories as $f) $price+=$f->getPrice();
+		return $price;
+	}
+	//========================================
+	//获取最终成交价格(不含运费)
+	public function getFinalPrice(){
+		$finalPrice=0;
+		//累加子订单的成交价格
+		foreach($this->childFactories as $f) $finalPrice+=$f->getFinalPrice();
+		return $finalPrice;
+	}
+	//========================================
+	//获取运费
+	public function getFreight(){
+		//不是会员
+		if($this->member->getLevel()==0){
+			//除了单买虚拟产品,其他情况一口价38
+			foreach($this->childFactories as $v) 
+				if($v->distributeType!=distribute::TYPE_VIRTUAL) return 38;
+			return 0;
+		}
+		//是会员
+		else{
+			//如果冷链不足2公斤收38,其他情况包邮
+			foreach($this->childFactories as $v)
+				if($v->distributeType==distribute::TYPE_REFRIGERATION) if($v->getWeight()<2000) return 38;
+			return 0;
+		}
+	}
+	//========================================
+	//获取订单标题
+	public function getTitle(){return NULL;}
+	//========================================
+	//索引,在整个订单树中唯一
+	public function getIndex(){return 'main';}
+}
