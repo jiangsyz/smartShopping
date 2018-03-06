e6a3e170d358447272c465cbf76b5a24293c429d
jiangsyz <305460883@qq.com>
2018-03-06 18:47:46 +0800

    add sourceProperty

---
+++ backend/models/source/sourceProperty.php
@@ -0,0 +1,8 @@
+<?php
+//资源属性
+namespace backend\models\source;
+use Yii;
+use yii\base\SmartException;
+use yii\db\SmartActiveRecord;
+//========================================
+class sourceProperty extends SmartActiveRecord{}
