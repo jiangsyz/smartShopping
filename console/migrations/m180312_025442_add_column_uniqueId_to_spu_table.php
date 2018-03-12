e1c56b83604363a187c0e679a084efefde48f9ed
jiangsyz <305460883@qq.com>
2018-03-12 11:08:52 +0800

    sql

---
+++ console/migrations/m180312_025442_add_column_uniqueId_to_spu_table.php
@@ -0,0 +1,8 @@
+<?php
+use yii\db\Migration;
+class m180312_025442_add_column_uniqueId_to_spu_table extends Migration{
+    public function up(){
+        $this->addColumn('spu','uniqueId','VARCHAR(200) NOT NULL COMMENT "spu唯一编号" AFTER `id`');
+        //$this->createIndex('spuUniqueId','spu','uniqueId',true);
+    }
+}
