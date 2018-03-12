01cfe9c641fd627a1c2fa8e091bba5811e0e0456
jiangsyz <305460883@qq.com>
2018-03-12 11:50:06 +0800

    sql修改

--- console/migrations/m180312_025454_add_column_uniqueId_to_sku_table.php
+++ console/migrations/m180312_025454_add_column_uniqueId_to_sku_table.php
@@ -3,6 +3,7 @@ use yii\db\Migration;
 class m180312_025454_add_column_uniqueId_to_sku_table extends Migration{
     public function up(){
         $this->addColumn('sku','uniqueId','VARCHAR(200) NOT NULL COMMENT "sku唯一编号" AFTER `id`');
-        //$this->createIndex('skuUniqueId','sku','uniqueId',true);
+        $this->execute("UPDATE `sku` SET `uniqueId`=`id` WHERE 1;");
+        $this->createIndex('skuUniqueId','sku','uniqueId',true);
     }
 }
