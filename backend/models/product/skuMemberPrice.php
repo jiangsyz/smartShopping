002f3791b931e757666a9d9f71d38e5c7c66d2d1
jiangsyz <305460883@qq.com>
2018-03-15 17:38:28 +0800

    skuMemberPrice增加校验

--- backend/models/product/skuMemberPrice.php
+++ backend/models/product/skuMemberPrice.php
@@ -4,5 +4,25 @@ namespace backend\models\product;
 use Yii;
 use yii\base\SmartException;
 use yii\db\SmartActiveRecord;
+use backend\models\model\source;
 //========================================
-class skuMemberPrice extends SmartActiveRecord{}
+class skuMemberPrice extends SmartActiveRecord{
+	//初始化
+	public function init(){
+		parent::init();
+		$this->on(self::EVENT_BEFORE_INSERT,array($this,"checkLevel"));
+		$this->on(self::EVENT_BEFORE_INSERT,array($this,"checkPrice"));
+		$this->on(self::EVENT_BEFORE_INSERT,array($this,"checkSku"));
+		$this->on(self::EVENT_BEFORE_UPDATE,array($this,"checkLevel"));
+		$this->on(self::EVENT_BEFORE_UPDATE,array($this,"checkPrice"));
+		$this->on(self::EVENT_BEFORE_UPDATE,array($this,"checkSku"));
+	}
+	//========================================
+	public function getSku(){return $this->hasOne(sku::className(),array('id'=>'skuId'));}
+	//========================================
+	public function checkSku(){if(!$this->sku) throw new SmartException("miss sku");}
+	//========================================
+	public function checkLevel(){if($this->lv<0) throw new SmartException("error lv");}
+	//========================================
+	public function checkPrice(){if($this->price<0) throw new SmartException("error price");}
+}
