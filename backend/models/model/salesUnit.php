1af61338ff844c35e7268b655ac4ee215d9e0bec
jiangsyz <305460883@qq.com>
2018-03-26 10:49:59 +0800

    salesUnit::getReduction()

--- backend/models/model/salesUnit.php
+++ backend/models/model/salesUnit.php
@@ -16,6 +16,9 @@ abstract class salesUnit extends product{
 	//获取针对某个会员等级的售卖价格
 	public function getLevelPrice($level){return $this->getPrice();}
 	//========================================
+	//获取成为会员后的节省金额
+	public function getReduction(){return $this->getLevelPrice(0)-$this->getLevelPrice(1);}
+	//========================================
 	//获取会员最终成交价格
 	abstract public function getFinalPrice(member $member);
 	//========================================
