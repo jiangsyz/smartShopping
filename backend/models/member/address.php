4a819d836d59b3439b4b703612c695705844bf85
jiangsyz <305460883@qq.com>
2018-03-26 16:48:53 +0800

    address.php修改

--- backend/models/member/address.php
+++ backend/models/member/address.php
@@ -5,4 +5,48 @@ use Yii;
 use yii\base\SmartException;
 use yii\db\SmartActiveRecord;
 //========================================
-class address extends SmartActiveRecord{}
+class address extends SmartActiveRecord{
+	//字段规则
+	public function rules(){
+	    return array(
+	        //去空格
+	        array(array('name','phone','address','postCode'),'trim'),
+	        //必填
+	        array(array('memberId','name','phone','areaId','address'),'required'),
+	        //唯一
+	        array(array(),'unique'),
+	    );
+	}
+	//========================================
+	//初始化
+	public function init(){
+		parent::init();
+		$this->on(self::EVENT_BEFORE_INSERT,array($this,"checkArea"));
+		$this->on(self::EVENT_BEFORE_INSERT,array($this,"checkMember"));
+		$this->on(self::EVENT_BEFORE_INSERT,array($this,"initCreateTime"));
+		$this->on(self::EVENT_BEFORE_UPDATE,array($this,"checkArea"));
+		$this->on(self::EVENT_BEFORE_UPDATE,array($this,"checkMember"));
+	}
+	//========================================
+	//初始化时间
+	public function initCreateTime(){$this->createTime=time();}
+	//========================================
+	//获取会员
+	public function getMember(){return $this->hasOne(member::className(),array('id'=>'memberId'));}
+	//========================================
+	//获取区域
+	public function getArea(){
+		return $this->hasOne(Yii::$app->smartArea->smartAreaRecord,array('area_id'=>'areaId'));
+	}
+	//========================================
+	//检查区域
+	public function checkArea(){
+		//区域必须存在
+		if(!$this->area) throw new SmartException("miss area");
+		//必须是第三级区域
+		if($this->area->level!=3) throw new SmartException("error area level");
+	}
+	//========================================
+	//检查会员
+	public function checkMember(){if(!$this->member) throw new SmartException("miss member");}
+}
