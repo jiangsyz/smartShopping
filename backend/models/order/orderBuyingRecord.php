b9390673bba9eb36673cb01d38298bd101be380b
jiangsyz <305460883@qq.com>
2018-03-26 16:04:09 +0800

    add backend/models/order/orderBuyingRecord.php

---
+++ backend/models/order/orderBuyingRecord.php
@@ -0,0 +1,8 @@
+<?php
+//订单购买行为记录
+namespace backend\models\order;
+use Yii;
+use yii\base\SmartException;
+use yii\db\SmartActiveRecord;
+//========================================
+class orderBuyingRecord extends SmartActiveRecord{}
