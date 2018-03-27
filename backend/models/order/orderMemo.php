90cee53ed6438fb4b54a9ba67ce10ec4c9a46e9d
jiangsyz <305460883@qq.com>
2018-03-27 23:45:50 +0800

    rm orderMemo.php

--- backend/models/order/orderMemo.php
+++
@@ -1,8 +0,0 @@
-<?php
-//订单备注信息
-namespace backend\models\order;
-use Yii;
-use yii\base\SmartException;
-use yii\db\SmartActiveRecord;
-//========================================
-class orderMemo extends SmartActiveRecord{}
