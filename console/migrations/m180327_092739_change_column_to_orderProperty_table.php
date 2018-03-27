1e47747c0c9314ac9334cf1f7d2f20487df50cfd
jiangsyz <305460883@qq.com>
2018-03-27 17:34:52 +0800

    sql

--- console/migrations/m180327_092739_change_column_to_orderProperty_table.php
+++ console/migrations/m180327_092739_change_column_to_orderProperty_table.php
@@ -1,6 +1,6 @@
 <?php
 use yii\db\Migration;
-class m180327_092739_change_column_to_orderMemo_table extends Migration{
+class m180327_092739_change_column_to_orderProperty_table extends Migration{
     public function up(){
         $this->dropIndex('orderPropertyUnique','order_property');
     }
