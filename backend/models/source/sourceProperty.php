dd8828a1054924ff7dfa25604900a35e3e417044
jiangsyz <305460883@qq.com>
2018-03-27 17:50:19 +0800

    sourceProperty.php initCreateTime

--- backend/models/source/sourceProperty.php
+++ backend/models/source/sourceProperty.php
@@ -5,4 +5,13 @@ use Yii;
 use yii\base\SmartException;
 use yii\db\SmartActiveRecord;
 //========================================
-class sourceProperty extends SmartActiveRecord{}
+class sourceProperty extends SmartActiveRecord{
+	//初始化
+	public function init(){
+		parent::init();
+		$this->on(self::EVENT_BEFORE_INSERT,array($this,"initCreateTime"));
+	}
+	//========================================
+	//初始化创建时间
+	public function initCreateTime(){$this->createTime=time();}
+}
