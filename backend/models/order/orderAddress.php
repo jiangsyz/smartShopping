3396d1a4a86e1ce04c2a8f57635dfd73be6781e7
jiangsyz <305460883@qq.com>
2018-03-27 23:42:58 +0800

    rm orderAddress.php

--- backend/models/order/orderAddress.php
+++
@@ -1,8 +0,0 @@
-<?php
-//订单地址信息
-namespace backend\models\order;
-use Yii;
-use yii\base\SmartException;
-use yii\db\SmartActiveRecord;
-//========================================
-class orderAddress extends SmartActiveRecord{}
