db57dc69ef8042e96df6964d1d346a76ef3b7f48
jiangsyz <305460883@qq.com>
2018-03-28 12:05:51 +0800

    订单地址管理器 getAddress()

--- backend/models/order/orderAddressManagement.php
+++ backend/models/order/orderAddressManagement.php
@@ -28,4 +28,27 @@ class orderAddressManagement extends Component{
 		//添加收货地址
 		$oRecord->propertyManagement->addProperty('address',json_encode($address->getData()));
 	}
+	//========================================
+	//获取地址
+	public function getAddress(){
+		//订单
+		$oRecord=$this->orderRecord;
+		//获取地址属性
+		$addressList=$oRecord->propertyManagement->getProperty('address');
+		//当前订单存在地址
+		if($addressList){
+			//每个订单只能有一个地址
+			if(count($addressList)!=1) 
+				throw new SmartException("order {$oRecord->id} error address count");
+			//返回地址
+			return $addressList[0];
+		}
+		//如果是主订单找不到地址,返回NULL
+		if(!$oRecord->parentId) return NULL;
+		//如果是子订单找不到地址,获取父订单地址
+		$parent=$oRecord->parentOrder;
+		if(!$parent) 
+			throw new SmartException("order {$oRecord->id} miss parent");
+		return $parent->addressManagement->getAddress();
+	}
 }
