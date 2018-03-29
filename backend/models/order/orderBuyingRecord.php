405ff443332a61eafee139a98f450ba2526ef514
jiangsyz <305460883@qq.com>
2018-03-29 17:04:02 +0800

    orderBuyingRecord插入前检查购买数量/插入后更新salesUnit库存

--- backend/models/order/orderBuyingRecord.php
+++ backend/models/order/orderBuyingRecord.php
@@ -4,5 +4,41 @@ namespace backend\models\order;
 use Yii;
 use yii\base\SmartException;
 use yii\db\SmartActiveRecord;
+use backend\models\model\source;
+use backend\models\order\orderRecord;
 //========================================
-class orderBuyingRecord extends SmartActiveRecord{}
+class orderBuyingRecord extends SmartActiveRecord{
+	//初始化
+	public function init(){
+		parent::init();
+		$this->on(self::EVENT_BEFORE_INSERT,array($this,"checkBuyingCount"));
+		$this->on(self::EVENT_AFTER_INSERT,array($this,"updateKeepCount"));
+	}
+	//========================================
+	//获取购买单元
+	public function getSalesUnit(){
+		return source::getSource($this->sourceType,$this->sourceId,true);
+	}
+	//========================================
+	//获取订单
+	public function getOrderRecord(){
+		return $this->hasOne(orderRecord::className(),array('id'=>'orderId'));
+	}
+	//========================================
+	//检查购买数量
+	public function checkBuyingCount(){
+		if($this->buyingCount<1) throw new SmartException("buyingCount < 1");	
+	}
+	//========================================
+	//修改库存
+	public function updateKeepCount(){
+		//获取购买单元
+		$salesUnit=$this->getSalesUnit();
+		if(!$salesUnit) throw new SmartException("miss salesUnit");
+		//修改库存
+		$handlerType=$this->orderRecord->getSourceType();
+		$handlerId=$this->orderRecord->getSourceId();
+		$keepCount=$salesUnit->getKeepCount()-$this->buyingCount;
+		$salesUnit->updateKeepCount($handlerType,$handlerId,$keepCount,$this->id);
+	}
+}
