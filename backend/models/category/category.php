ce47af1eb1813178833783e2ec38c098eea718c8
jiangsyz <305460883@qq.com>
2018-03-30 00:55:00 +0800

    category获取后代分类

--- backend/models/category/category.php
+++ backend/models/category/category.php
@@ -9,8 +9,20 @@ class category extends SmartActiveRecord{
 	//获取子分类
 	public function getChildren(){return $this->hasMany(self::className(),array('pid'=>'id'));}
 	//========================================
+	//获取后代分类
+	public function getPosterities(){
+		//用栈代替递归,寻找直接子分类和间接子分类
+		$checkList=$posterities=$this->children;
+		while(!empty($checkList)){
+			$check=array_shift($checkList);
+			foreach($check->children as $c) $checkList[]=$posterities[]=$c;
+		}
+		return $posterities;
+	}
+	//========================================
 	//获取所有的顶级分类
 	static public function getTopCategories(){
 		return self::find()->where("`pid` is NULL")->orderBy("`sort` ASC")->all();
 	}
+
 }
