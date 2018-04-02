1a35f7e6c80f98a40ddbb51bf8c04f030ca6e2c1
jiangsyz <305460883@qq.com>
2018-04-02 17:50:06 +0800

    获取订单列表

--- backend/controllers/OrderController.php
+++ backend/controllers/OrderController.php
@@ -7,8 +7,7 @@ use yii\base\Exception;
 use backend\models\model\source;
 use backend\models\member\member;
 use backend\models\token\tokenManagement;
-use backend\models\signInManagement\signInManagement;
-use backend\models\identifyingCode\identifyingCodeManagement;
+use backend\models\order\orderRecord;
 class OrderController extends SmartWebController{
 	//获取会员订单统计
 	public function actionApiGetOrderStatistics(){
@@ -20,8 +19,53 @@ class OrderController extends SmartWebController{
 		$data['unpaid']=100;
 		$data['undelivered']=200;
 		$data['unreceipted']=300;
-		$data['customerService']=400;
+		$data['re']=400;
 		//返回
 		$this->response(1,array('error'=>0,'data'=>$data));
+	}
+	//========================================
+	public function actionApiGetOrders(){
+		try{
+			//根据token获取会员
+			$token=Yii::$app->request->get('token',false);
+			$member=tokenManagement::getManagement($token,array(source::TYPE_MEMBER))->getOwner();
+			//获取状态
+			$status=Yii::$app->request->get('status',0);
+			//获取每页多少条
+			$pageSize=Yii::$app->request->get('pageSize',0);
+			//获取当前第几页
+			$pageNum=Yii::$app->request->get('pageNum',0);
+			//根据不同状态取sql
+			$sql=false;
+			if($status=='all') $sql=$this->getAllOrders();
+			if($status=='unpaid') $sql=$this->getUnpaidOrders();
+			if(!$sql) throw new SmartException("error status");
+			//查询query
+			$query=orderRecord::findBySql($sql);
+			//获取分页数据
+			$result=Yii::$app->smartPagination->getData($query,$pageSize,$pageNum);
+			//组织数据
+			$data=$result;
+			unset($data['objs']);
+			$data['orders']=array();
+			foreach($result['objs'] as $order){
+				$data['orders'][]=$order->extraction->getBasicData();
+			}
+			//返回
+			$this->response(1,array('error'=>0,'data'=>$data));
+    	}
+    	catch(Exception $e){$this->response(1,array('error'=>-1,'msg'=>$e->getMessage()));}
+	}
+	//========================================
+	//获取待支付订单
+	public function getUnpaidOrders(){
+		$table=orderRecord::tableName();
+		return "SELECT * FROM {$table} WHERE `parentId` is NULL AND `payStatus`='0' AND `cancelStatus`='0' ORDER BY `createTime` DESC";
+	}
+	//========================================
+	//获取全部订单
+	public function getAllOrders(){
+		$table=orderRecord::tableName();
+		return "SELECT * FROM {$table} WHERE `parentId` is NULL ORDER BY `createTime` DESC";
 	}
 }
