1b504b531560802e15622f428a9aba22b8acaeb5
jiangsyz <305460883@qq.com>
2018-04-03 15:21:03 +0800

    virtualItemDetail

---
+++ backend/models/product/virtualItemDetail.php
@@ -0,0 +1,8 @@
+<?php
+//虚拟售卖单元
+namespace backend\models\product;
+use Yii;
+use yii\base\SmartException;
+use yii\db\SmartActiveRecord;
+//========================================
+class virtualItemDetail extends SmartActiveRecord{}
