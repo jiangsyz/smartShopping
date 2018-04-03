6f295f92309d6caf624617cc81cc7213fa631736
jiangsyz <305460883@qq.com>
2018-04-03 11:44:16 +0800

    backend/models/pay/

---
+++ backend/models/pay/payCallback.php
@@ -0,0 +1,8 @@
+<?php
+//支付回调
+namespace backend\models\pay;
+use Yii;
+use yii\base\SmartException;
+use yii\db\SmartActiveRecord;
+//========================================
+class payCallback extends SmartActiveRecord{}
