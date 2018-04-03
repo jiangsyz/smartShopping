a23f953d3982d940583908cf337fcdeca92520dc
jiangsyz <305460883@qq.com>
2018-04-03 11:57:11 +0800

    orderStatusManagement checkPayTimeout()/获取状态前checkPayTimeout()

--- backend/models/order/orderStatusManagement.php
+++ backend/models/order/orderStatusManagement.php
@@ -6,24 +6,45 @@ use yii\base\SmartException;
 use yii\base\Component;
 //========================================
 class orderStatusManagement extends Component{
+	const PAY_TIME_OUT=60*5;//支付超时秒数
+	//========================================
 	const STATUS_UNPAID=1;//待支付状态
-	const STATUS_REFUNDING=2;//退款中
-	const STATUS_CLOSED=3;//交易关闭
+	const STATUS_REFUNDING=2;//退款中状态
+	const STATUS_CLOSED=3;//交易关闭状态
 	//========================================
 	//订单记录
 	public $orderRecord=NULL;
 	//========================================
+	//检查支付是否超时
+	public function checkPayTimeout(){
+		//只有主订单才有状态的概念
+		if($this->orderRecord->parentId) throw new SmartException("is not main order");
+		//只检查支付状态为待支付的订单
+		if($this->orderRecord->payStatus!=0) return;
+		//计算等待支付时常
+		$waiting=time()-$this->orderRecord->createTime;
+		//超时了修改支付状态,不能被支付
+		if($waiting>self::PAY_TIME_OUT)
+			$this->orderRecord->updateObj(array('payStatus'=>-1));
+	}
+	//========================================
 	//获取状态
 	public function getStatus(){
+		//只有主订单才有状态的概念
+		if($this->orderRecord->parentId) throw new SmartException("is not main order");
+		//检查支付是否超时
+		$this->checkPayTimeout();
 		//退款状态(一定要先判断这个状态,可以减少不必要的重复计算)
-		if($this->isRefunding()) return self::STATUS_REFUNDING;
+		if($this->isRefunding()) 
+			return self::STATUS_REFUNDING;
 		//交易关闭状态
-		elseif($this->isClosed()) return self::STATUS_CLOSED;
+		elseif($this->isClosed()) 
+			return self::STATUS_CLOSED;
 		//未支付状态
-		elseif($this->isUnpaid()) return self::STATUS_UNPAID;
-		else throw new SmartException("error order status");
-		
-		
+		elseif($this->isUnpaid()) 
+			return self::STATUS_UNPAID;
+		else 
+			throw new SmartException("error order status");
 	}
 	//========================================
 	//判断是否为待支付状态
