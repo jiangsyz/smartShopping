de740d3eb6a1bd9d3db620d4314cefa020d2d401
jiangsyz <305460883@qq.com>
2018-04-04 16:02:14 +0800

    订单状态管理器修改

--- backend/models/order/orderStatusManagement.php
+++ backend/models/order/orderStatusManagement.php
@@ -8,70 +8,79 @@ use yii\base\Component;
 class orderStatusManagement extends Component{
 	const PAY_TIME_OUT=60*5;//支付超时秒数
 	//========================================
-	const STATUS_UNPAID=1;//待支付状态
-	const STATUS_REFUNDING=2;//退款中状态
-	const STATUS_CLOSED=3;//交易关闭状态
+	const STATUS_UNPAID=1;//待支付
+	const STATUS_REFUNDING=2;//退款中
+	const STATUS_CLOSED=3;//交易关闭
 	const STATUS_UNDELIVERED=4;//待发货
+	const STATUS_UNRECEIPTED=5;//待收货
+	const STATUS_FINISHED=6;//已完成
 	//========================================
 	//订单记录
 	public $orderRecord=NULL;
 	//========================================
+	//获取订单状态
+	public function getStatus(){
+		//检查支付状态
+		$this->checkPayTimeout();
+		//检查状态
+		$this->checkStatus();
+		//退款中
+		if($this->orderRecord->refundingStatus==1) return self::STATUS_REFUNDING;
+		//交易关闭
+		elseif($this->orderRecord->closeStatus==1) return self::STATUS_CLOSED;
+		elseif($this->orderRecord->cancelStatus==1) return self::STATUS_CLOSED;
+		elseif($this->orderRecord->payStatus==-1) return self::STATUS_CLOSED;
+		//已完成
+		elseif($this->orderRecord->finishStatus==1) return self::STATUS_FINISHED;
+		elseif($this->orderRecord->deliverStatus==3) return self::STATUS_FINISHED;
+		//待收货
+		elseif($this->orderRecord->deliverStatus==1) return self::STATUS_UNRECEIPTED;
+		elseif($this->orderRecord->deliverStatus==2) return self::STATUS_UNRECEIPTED;
+		//待支付或待发货
+		elseif($this->orderRecord->deliverStatus==0){
+			//待支付
+			if($this->orderRecord->payStatus==0) return self::STATUS_UNPAID;
+			//待发货
+			if($this->orderRecord->payStatus==1){
+				//非虚拟订单,待发货
+				if($this->orderRecord->isNeedAddress) return self::STATUS_UNDELIVERED;
+				//虚拟订单,已完成
+				else{
+					$this->orderRecord->updateObj(array('deliverStatus'=>3,'finishStatus'=>1));
+					return self::STATUS_FINISHED;
+				}
+			}
+		}
+		//错误状态
+		else throw new SmartException("error order status");
+	}
+	//========================================
 	//检查支付是否超时
 	public function checkPayTimeout(){
-		//只有主订单才有状态的概念
-		if($this->orderRecord->parentId) throw new SmartException("is not main order");
-		//只检查支付状态为待支付的订单
+		//不是主订单不处理
+		if($this->orderRecord->parentId) return;
+		//不是未支付不处理
 		if($this->orderRecord->payStatus!=0) return;
+		//取消或关闭的不处理
+		if($this->orderRecord->cancelStatus==1) return;
+		if($this->orderRecord->closeStatus==1) return;
 		//计算等待支付时常
 		$waiting=time()-$this->orderRecord->createTime;
-		//超时了修改支付状态,不能被支付
-		if($waiting>self::PAY_TIME_OUT)
-			$this->orderRecord->updateObj(array('payStatus'=>-1));
+		//超时了修改支付状态
+		if($waiting>self::PAY_TIME_OUT) $this->orderRecord->updateObj(array('payStatus'=>-1));
 	}
 	//========================================
-	//获取状态
-	public function getStatus(){
+	//检查状态
+	public function checkStatus(){
+		$r=$this->orderRecord;
 		//只有主订单才有状态的概念
-		if($this->orderRecord->parentId) throw new SmartException("is not main order");
-		//检查支付是否超时
-		$this->checkPayTimeout();
-		//退款状态(一定要先判断这个状态,可以减少不必要的重复计算)
-		if($this->isRefunding()) 
-			return self::STATUS_REFUNDING;
-		//交易关闭状态
-		elseif($this->isClosed()) 
-			return self::STATUS_CLOSED;
-		//未支付状态
-		elseif($this->isUnpaid()) 
-			return self::STATUS_UNPAID;
-		//待发货状态
-		elseif($this->isUndelivered()) 
-			return self::STATUS_UNDELIVERED;
-		else 
-			throw new SmartException("error order status");
-	}
-	//========================================
-	//判断是否为待支付状态
-	public function isUnpaid(){
-		if($this->isClosed()) return false;
-		elseif($this->orderRecord->payStatus==0) return true;
-		else return false;
-	}
-	//========================================
-	//判断是否为交易关闭状态
-	public function isClosed(){
-		if($this->isRefunding()) return false;
-		elseif($this->orderRecord->cancelStatus==1) return true;
-		elseif($this->orderRecord->closeStatus==1) return true;
-		elseif($this->orderRecord->payStatus==-1) return true;
-		else return false;
-	}
-	//========================================
-	//判断是否为退款中状态
-	public function isRefunding(){return false;}
-	//========================================
-	//判断是否为待发货状态
-	public function isUndelivered(){
-		if($this->orderRecord->payStatus==1) return true; else return false;
+		if($r->parentId) throw new SmartException("status error:0");
+		//不能共存的指标
+		if($r->finishStatus==1 && $r->refundingStatus==1) throw new SmartException("status error:1");
+		if($r->finishStatus==1 && $r->closeStatus==1) throw new SmartException("status error:2");
+		if($r->finishStatus==1 && $r->cancelStatus==1) throw new SmartException("status error:3");
+		if($r->finishStatus==1 && $r->payStatus==－1) throw new SmartException("status error:4");
+		if($r->finishStatus==1 && $r->deliverStatus!=3) throw new SmartException("status error:5");
+		if($r->deliverStatus>0 && $r->payStatus!=1) throw new SmartException("status error:6");
 	}
 }
