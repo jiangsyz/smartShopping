316e8db5cf55dc4eef0a9ccd3f69de5181ef6285
jiangsyz <305460883@qq.com>
2018-04-08 15:25:42 +0800

    orderExtraction::getDetail

--- backend/models/order/orderExtraction.php
+++ backend/models/order/orderExtraction.php
@@ -38,6 +38,50 @@ class orderExtraction extends Component{
 			$bInfo['buyingCount']=$b->buyingCount;
 			$data['buyingRecords'][]=$bInfo;
 		}
+		//返回数据
+		return $data;
+	}
+	//========================================
+	//获取订单详情
+	public function getDetail(){
+		$data=array();
+		//订单编号
+		$data['id']=$this->orderRecord->id;
+		//对外美化的订单编号
+		$data['showId']=$this->getShowId();
+		//订单标题
+		$data['title']=$this->orderRecord->title;
+		//父订单id
+		$data['parentId']=$this->orderRecord->parentId;
+		//支付价格
+		$data['pay']=formatPrice::formatPrice($this->orderRecord->pay/100);
+		//商品价格
+		$data['finalPrice']=formatPrice::formatPrice($this->orderRecord->finalPrice);
+		//运费
+		$data['freight']=formatPrice::formatPrice($this->orderRecord->freight);
+		//状态
+		$data['status']=$this->orderRecord->statusManagement->getStatus();
+		//是否需要地址
+		$data['isNeedAddress']=$this->orderRecord->isNeedAddress;
+		//地址信息
+		$data['address']=$this->orderRecord->addressManagement->getAddress();
+		//备注信息
+		$data['memo']=$this->orderRecord->memoManagement->getMemo(1);
+		//获取购买行为
+		$data['buyingRecords']=array();
+		$childOrders=$this->orderRecord->relationManagement->getChildren();
+		foreach($childOrders as $c){
+			$key=$c->title;
+			if(isset($data['buyingRecords'][$key])) throw new SmartException("title existed");
+			$data['buyingRecords'][$key]=array();
+			$buyingRecords=$c->buyingManagement->getBuyingRecords();
+			foreach($buyingRecords as $b){
+				$bInfo=$b->getSalesUnit()->getExtraction()->getBasicData($this->orderRecord->member);
+				$bInfo['buyingCount']=$b->buyingCount;
+				$data['buyingRecords'][$key][]=$bInfo;
+			}
+		}
+		//返回数据
 		return $data;
 	}
 }
