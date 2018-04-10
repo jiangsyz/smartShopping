d76caf643a6e40852383595ab9987af95477fcda
jiangsyz <305460883@qq.com>
2018-04-10 16:31:50 +0800

    notice model

---
+++ backend/models/notice/notice.php
@@ -0,0 +1,8 @@
+<?php
+//订单记录
+namespace backend\models\notice;
+use Yii;
+use yii\base\SmartException;
+use yii\db\SmartActiveRecord;
+//========================================
+class notice extends SmartActiveRecord{}
