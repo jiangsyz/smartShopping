c1d9bb0c9809db64bd6712aa96387af96efc19c6
jiangsyz <305460883@qq.com>
2018-04-11 16:01:07 +0800

    完善notice

--- backend/models/notice/notice.php
+++ backend/models/notice/notice.php
@@ -4,5 +4,29 @@ namespace backend\models\notice;
 use Yii;
 use yii\base\SmartException;
 use yii\db\SmartActiveRecord;
+use backend\models\member\member;
 //========================================
-class notice extends SmartActiveRecord{}
+class notice extends SmartActiveRecord{
+	const TYPE_PAY=1;
+	const TYPE_VIP=2;
+	//========================================
+	//初始化
+	public function init(){
+		parent::init();
+		$this->on(self::EVENT_BEFORE_INSERT,array($this,"initData"));
+		$this->on(self::EVENT_BEFORE_INSERT,array($this,"checkMember"));
+	}
+	//========================================
+	//初始化数据
+	public function initData(){
+		$this->createTime=time();
+		$this->sendStatus=0;
+		$this->sendTime=0;
+	}
+	//========================================
+	//检查会员
+	public function checkMember(){if(!$this->member) throw new SmartException("miss member");}
+	//========================================
+	//返回会员
+	public function getMember(){return $this->hasOne(member::className(),array('id'=>'memberId'));}
+}
