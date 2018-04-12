d0b92ea083ecfaa6c6566ece536d0ffda5875f59
jiangsyz <305460883@qq.com>
2018-04-12 17:09:56 +0800

    notice::push()

--- backend/models/notice/notice.php
+++ backend/models/notice/notice.php
@@ -1,5 +1,5 @@
 <?php
-//订单记录
+//通知
 namespace backend\models\notice;
 use Yii;
 use yii\base\SmartException;
@@ -29,4 +29,27 @@ class notice extends SmartActiveRecord{
 	//========================================
 	//返回会员
 	public function getMember(){return $this->hasOne(member::className(),array('id'=>'memberId'));}
+	//========================================
+	//返回下一跳
+	public function getNextStep(){return "noticeList";}
+	//========================================
+	//推送
+	public function push(){
+		if($this->sendStatus!=0) throw new SmartException("error sendStatus");
+		//先标记成出错
+		$this->updateObj(array('sendStatus'=>-1,'sendTime'=>time()));
+		//获取会员推送数据
+		$pushUniqueInfo=explode(',',$this->member->pushUniqueId);
+		if(!isset($pushUniqueInfo[0])) throw new SmartException("miss pushUniqueId");
+		if(!isset($pushUniqueInfo[1])) throw new SmartException("miss appType");
+		//推送
+		$msg=array();
+		$msg['title']=$this->content;
+		$msg['msg_content']=$this->content;
+		$msg['content_type']='text';
+		$msg['extras']=array('nextStep'=>$this->getNextStep());
+		Yii::$app->smartPush->pushByRegistrationId($pushUniqueInfo[1],$pushUniqueInfo[0],$msg);
+		//标记成功
+		$this->updateObj(array('sendStatus'=>1));
+	}
 }
