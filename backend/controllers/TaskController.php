a8e4afc00299146c325135d6d78c354c9ac3c0ed
jiangsyz <305460883@qq.com>
2018-04-12 12:00:39 +0800

    定时任务

--- backend/controllers/TaskController.php
+++ backend/controllers/TaskController.php
@@ -22,7 +22,20 @@ class TaskController extends SmartWebController{
 			$trascation=Yii::$app->db->beginTransaction();
 			//取一个最旧的未支付的订单id
 			$table=orderRecord::tableName();
-			$sql="SELECT `id` FROM {$table} WHERE `parentId` is NULL AND `payStatus`='0' AND `cancelStatus`='0' ORDER BY `createTime` ASC LIMIT 1;";
+			$sql=
+			"
+					SELECT 
+						`id` 
+					FROM 
+						{$table} 
+					WHERE 
+						`parentId` is NULL 
+						AND 
+						`payStatus`='0' 
+						AND 
+						`cancelStatus`='0' 
+					ORDER BY `createTime` ASC LIMIT 1;
+			";
 			$row=Yii::$app->db->createCommand($sql)->queryOne();
 			//没取到id(这里虽然抛异常,但也不算错,没有需要处理而已)
 			if(!isset($row['id'])) throw new SmartException("miss id");
@@ -31,6 +44,47 @@ class TaskController extends SmartWebController{
 			if(!$orderRecord) throw new SmartException("miss order {$row['id']}");
 			//检查超时
 			$orderRecord->payManagement->canOrderPay();
+			//提交事务
+			$trascation->commit();
+			//返回
+			$this->response(1,array('error'=>0,'data'=>$orderRecord->id));
+		}
+		catch(Exception $e){
+			//回滚
+			$trascation->rollback();
+			$this->response(1,array('error'=>-1,'msg'=>$e->getMessage()));
+    	}
+	}
+	//========================================
+	//订单超时/取消反库存
+	public function actionApiBackKeepCount(){
+		try{
+			//开启事务
+			$trascation=Yii::$app->db->beginTransaction();
+			//获取一个最旧的支付超时或取消的且未退库存的订单
+			$table=orderRecord::tableName();
+			$sql=
+			"
+					SELECT 
+						`id` 
+					FROM 
+						{$table} 
+					WHERE 
+						`parentId` is NULL 
+						AND 
+						(`payStatus`='-1' OR `cancelStatus`='1') 
+						AND 
+						`backKeepCountStatus`='0' 
+					ORDER BY `createTime` ASC LIMIT 1;
+			";
+			$row=Yii::$app->db->createCommand($sql)->queryOne();
+			//没取到id(这里虽然抛异常,但也不算错,没有需要处理而已)
+			if(!isset($row['id'])) throw new SmartException("miss id");
+			//加锁取订单
+			$orderRecord=orderRecord::getLockedOrderById($row['id']);
+			if(!$orderRecord) throw new SmartException("miss order {$row['id']}");
+			//返库存
+			$orderRecord->cancelManagement->backKeepCount();
 			//提交事务
 			$trascation->commit();
 			//返回
