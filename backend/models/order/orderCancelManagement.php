1efe3f84ffd76f4c4a153d709ee31ad25bc00598
jiangsyz <305460883@qq.com>
2018-04-12 11:59:51 +0800

    orderCancelManagement::backKeepCount

--- backend/models/order/orderCancelManagement.php
+++ backend/models/order/orderCancelManagement.php
@@ -4,6 +4,7 @@ namespace backend\models\order;
 use Yii;
 use yii\base\SmartException;
 use yii\base\Component;
+use yii\base\models\source;
 //========================================
 class orderCancelManagement extends Component{
 	//订单记录
@@ -27,6 +28,23 @@ class orderCancelManagement extends Component{
 		//不允许处理
 		else 
 			throw new SmartException("该订单状态不能取消",-2);
+	}
+	//========================================
+	//返回库存
+	public function backKeepCount(){
+		if($this->orderRecord->parentId) 
+			throw new SmartException("该订单不是主订单",-2);
+		if(!($this->orderRecord->payStatus==-1 || $this->orderRecord->cancelStatus==1))
+			throw new SmartException("该订单状态不允许返回库存",-2);
+		//已经退过
+		if($this->orderRecord->backKeepCountStatus!=0) 
+			throw new SmartException("重复处理",-2);
+		//获取购买记录
+		$buyingRecords=$this->orderRecord->buyingManagement->getBuyingList();
+		//返回库存
+		foreach($buyingRecords as $buyingRecord) $buyingRecord->backKeepCount();
+		//标记已返还
+		$this->orderRecord->updateObj(array('backKeepCountStatus'=>1));
 	}
 	//========================================
 }
