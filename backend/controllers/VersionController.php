27e698f635643c4fe539f6959b3d1c8762b289b8
jiangsyz <305460883@qq.com>
2018-04-13 16:53:14 +0800

    获取版本信息api

---
+++ backend/controllers/VersionController.php
@@ -0,0 +1,28 @@
+<?php
+namespace backend\controllers;
+use Yii;
+use yii\web\SmartWebController;
+use yii\base\Exception;
+use yii\base\SmartException;
+use backend\models\version\version;
+class VersionController extends SmartWebController{
+    public function actionApiGetVersion(){
+        try{
+            //开启事务
+            $trascation=Yii::$app->db->beginTransaction();
+            //获取客户端类型
+            $type=Yii::$app->request->get('type',false);
+            //获取最新版本信息
+            $version=version::find()->where("`type`='Android'")->orderBy("`versionTime` DESC")->one();
+            if(!$version) throw new SmartException("miss version");
+            //提交事务
+            $trascation->commit();
+            $this->response(1,array('error'=>0,'data'=>$version->getData()));
+        }
+        catch(Exception $e){
+            //回滚
+            $trascation->rollback();
+            $this->response(1,array('error'=>-1,'msg'=>$e->getMessage()));
+        }
+    }
+}
---
+++ backend/models/version/version.php
@@ -0,0 +1,8 @@
+<?php
+//幻灯片
+namespace backend\models\version;
+use Yii;
+use yii\base\SmartException;
+use yii\db\SmartActiveRecord;
+//========================================
+class version extends SmartActiveRecord{}
