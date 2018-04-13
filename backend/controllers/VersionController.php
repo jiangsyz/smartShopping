921bcf11dc01b474ca6775ffe9b6c7ec303f2053
jiangsyz <305460883@qq.com>
2018-04-13 17:37:41 +0800

    版本api

--- backend/controllers/VersionController.php
+++ backend/controllers/VersionController.php
@@ -14,10 +14,10 @@ class VersionController extends SmartWebController{
             $type=Yii::$app->request->get('type',false);
             //获取最新版本信息
             $version=version::find()->where("`type`='Android'")->orderBy("`versionTime` DESC")->one();
-            if(!$version) throw new SmartException("miss version");
+            if(!$version) throw new SmartException("版本信息异常",-2);
             //提交事务
             $trascation->commit();
-            $this->response(1,array('error'=>0,'data'=>$version->getData()));
+            $this->response(1,array('error'=>0,'data'=>$version->getData(array('type','versionName','memo'))));
         }
         catch(Exception $e){
             //回滚
