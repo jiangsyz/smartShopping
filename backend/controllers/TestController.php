e19833d05aeffc877d4f4ba520e6a01e6f2803e1
jiangsyz <305460883@qq.com>
2018-04-13 15:28:30 +0800

    获取服务器时间戳

--- backend/controllers/TestController.php
+++ backend/controllers/TestController.php
@@ -9,8 +9,18 @@ use backend\models\product\virtualItem;
 use backend\models\orderFactory\buyingRecord;
 use backend\models\member\member;
 use backend\models\token\tokenManagement;
+use backend\models\notice\notice;
+use yii\base\Exception;
 class TestController extends SmartWebController{
-    public function actionIndex(){
-    	echo 111;
+    public function actionIndex(){echo time();}
+    //========================================
+    public function actionTest(){
+    	$get=array("phone"=>"13402155751","requestTime"=>time());
+    	$get['requestTime']=(string)$get['requestTime'];
+    	ksort($get);
+    	var_dump(json_encode($get)."^ZS2018LCJ");
+    	$signature=md5(json_encode($get)."^ZS2018LCJ");
+    	$url="http://localhost/smartShopping/backend/web/index.php?r=member/api-get-token-by-phone&phone={$get['phone']}&requestTime={$get['requestTime']}&signature={$signature}";
+    	echo $url;
     }
 }
