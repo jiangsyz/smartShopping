ef4abaa0251bd4308d08cf93bb106701c0319b80
jiangsyz <305460883@qq.com>
2018-05-04 15:31:24 +0800

    修改refund::initData()/去掉refund::applyRefund()

--- backend/models/order/refund.php
+++ backend/models/order/refund.php
@@ -4,6 +4,7 @@ namespace backend\models\order;
 use Yii;
 use yii\base\SmartException;
 use yii\db\SmartActiveRecord;
+use backend\models\model\source;
 //========================================
 class refund extends SmartActiveRecord{
 	//初始化
@@ -15,27 +16,14 @@ class refund extends SmartActiveRecord{
 	//初始化数据
 	public function initData(){
 		$this->applyTime=time();
+		$this->rejectHandlerType=NULL;
+		$this->rejectHandlerId=NULL;
+		$this->rejectTime=NULL;
+		$this->rejectMemo=NULL;
+		$this->refundHandlerType=NULL;
+		$this->refundHandlerId=NULL;
+		$this->refundTime=NULL;
+		$this->refundMemo=NULL;
 		$this->status=0;
-		$this->rejectMemo="";
-		$this->refundMemo="";
-	}
-	//========================================
-	//申请一个退款
-	public static function applyRefund(orderRecord $orderRecord,$data){
-		//订单没付过钱的不能退
-		if($orderRecord->payStatus!=1) throw new SmartException("未支付订单不能申请退款",-2);
-		//校验
-		if(!isset($data['bid'])) throw new SmartException("miss bid");
-		if(!isset($data['price'])) throw new SmartException("miss price");
-		if(!isset($data['applyMemo'])) throw new SmartException("miss applyMemo");
-		//添加退款记录
-		$refundData=array();
-		$refundData['oid']=$orderRecord->id;
-		$refundData['bid']=$data['bid'];
-		$refundData['price']=$data['price'];
-		$refundData['applyMemo']=$data['applyMemo'];
-		self::addObj($refundData);
-		//修改订单相关数据
-		$orderRecord->updateObj(array('refundingStatus'=>1,'finishStatus'=>0));
 	}
 }
