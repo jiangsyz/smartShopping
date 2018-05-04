27f67c708218739b8aec451c4a00613091d747cc
jiangsyz <305460883@qq.com>
2018-05-04 15:35:54 +0800

    orderCancelManagement修改cancel/backKeepCount;增加close

--- backend/models/order/orderCancelManagement.php
+++ backend/models/order/orderCancelManagement.php
@@ -5,6 +5,7 @@ use Yii;
 use yii\base\SmartException;
 use yii\base\Component;
 use yii\base\models\source;
+use backend\models\staff\staff;
 //========================================
 class orderCancelManagement extends Component{
 	//订单记录
@@ -14,37 +15,56 @@ class orderCancelManagement extends Component{
 	public function cancel(){
 		//获取状态
 		$status=$this->orderRecord->statusManagement->getStatus();
-		//处理
+		//未支付订单直接标记取消即可
 		if($status==orderStatusManagement::STATUS_UNPAID) 
 			$this->orderRecord->updateObj(array('cancelStatus'=>1));
+		//待发货订单先标记取消后申请整单退款
 		elseif($status==orderStatusManagement::STATUS_UNDELIVERED){
 			$this->orderRecord->updateObj(array('cancelStatus'=>1));
-			$refund=array();
-			$refund['bid']=0;
-			$refund['price']=$this->orderRecord->pay;
-			$refund['applyMemo']="整单取消";
-			refund::applyRefund($this->orderRecord,$refund);
+			$refundManagement=$this->orderRecord->refundManagement;
+			$refundManagement->allpyRefundByOrder($this->orderRecord->member,$this->orderRecord->pay,"会员整单取消");
 		}
 		//不允许处理
 		else 
 			throw new SmartException("该订单状态不能取消",-2);
 	}
 	//========================================
+	//订单关闭
+	public function close(staff $staff){
+		//获取状态
+		$status=$this->orderRecord->statusManagement->getStatus();
+		//未支付订单直接标记取消即可
+		if($status==orderStatusManagement::STATUS_UNPAID) 
+			$this->orderRecord->updateObj(array('closeStatus'=>1));
+		//待发货订单先标记取消后申请整单退款
+		elseif($status==orderStatusManagement::STATUS_UNDELIVERED){
+			$this->orderRecord->updateObj(array('closeStatus'=>1));
+			$refundManagement=$this->orderRecord->refundManagement;
+			$refundManagement->allpyRefundByOrder($staff,$this->orderRecord->pay,"员工整单关闭");
+		}
+		//不允许处理
+		else 
+			throw new SmartException("该订单状态不能关闭",-2);
+	}
+	//========================================
 	//返回库存
 	public function backKeepCount(){
-		if($this->orderRecord->parentId) 
+		$orderRecord=$this->orderRecord;
+		//只处理主订单
+		if($orderRecord->parentId) 
 			throw new SmartException("该订单不是主订单",-2);
-		if(!($this->orderRecord->payStatus==-1 || $this->orderRecord->cancelStatus==1))
+		//只处理支付失败/取消/关闭的订单
+		if(!($orderRecord->payStatus==-1 || $orderRecord->cancelStatus==1 || $orderRecord->closeStatus==1))
 			throw new SmartException("该订单状态不允许返回库存",-2);
-		//已经退过
-		if($this->orderRecord->backKeepCountStatus!=0) 
+		//不重复处理
+		if($orderRecord->backKeepCountStatus!=0) 
 			throw new SmartException("重复处理",-2);
 		//获取购买记录
-		$buyingRecords=$this->orderRecord->buyingManagement->getBuyingList();
+		$buyingRecords=$orderRecord->buyingManagement->getBuyingList();
 		//返回库存
 		foreach($buyingRecords as $buyingRecord) $buyingRecord->backKeepCount();
 		//标记已返还
-		$this->orderRecord->updateObj(array('backKeepCountStatus'=>1));
+		$orderRecord->updateObj(array('backKeepCountStatus'=>1));
 	}
 	//========================================
 }
