8c8d5b42d824a4e7d2cb2fa39a146ed85c79b7ae
jiangsyz <305460883@qq.com>
2018-05-04 18:58:32 +0800

    驳回退款api

---
+++ backend/controllers/RefundController.php
@@ -0,0 +1,40 @@
+<?php
+namespace backend\controllers;
+use Yii;
+use yii\web\SmartWebController;
+use yii\base\SmartException;
+use yii\base\Exception;
+use backend\models\model\source;
+use backend\models\token\tokenManagement;
+class RefundController extends SmartWebController{
+	//驳回退款
+	public function actionApiReject(){
+		try{
+			//开启事务
+			$trascation=Yii::$app->db->beginTransaction();
+			//根据token获取员工
+			$token=Yii::$app->request->post('token',false);
+			$staff=tokenManagement::getManagement($token,array(source::TYPE_STAFF))->getOwner();
+			//获取订单id
+			$orderId=Yii::$app->request->post('orderId',0);
+			//获取退款记录id
+			$refundId=Yii::$app->request->post('refundId',0);
+			//获取备注
+			$memo=Yii::$app->request->post('memo',0);
+			//获取订单
+			$orderRecord=orderRecord::getLockedOrderById($orderId);
+			if(!$orderRecord) throw new SmartException("miss orderRecord");
+			//取消订单
+			$orderRecord->refundManagement->reject($staff,$refundId,$memo);
+			//提交事务
+			$trascation->commit();
+			//返回
+			$this->response(1,array('error'=>0));
+		}
+		catch(Exception $e){
+			//回滚
+			$trascation->rollback();
+			$this->response(1,array('error'=>$e->getCode()?$e->getCode():-1,'msg'=>$e->getMessage()));
+    	}
+	}
+}
