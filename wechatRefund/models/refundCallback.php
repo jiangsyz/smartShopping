7ce2ba1602d1413664dcaa82c2b35a686e2ed771
jiangsyz <305460883@qq.com>
2018-05-21 14:39:25 +0800

    refundCallback

---
+++ wechatRefund/models/refundCallback.php
@@ -0,0 +1,11 @@
+<?php
+//退款回调
+namespace wechatRefund\models\pay;
+use Yii;
+use yii\base\SmartException;
+use yii\db\SmartActiveRecord;
+//========================================
+class refundCallback extends SmartActiveRecord{
+	//指定库
+	public static function getDb(){return Yii::$app->log_db;}
+}
