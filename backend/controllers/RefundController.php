5118ae8f1b1714244e131d1f2bb0499db3de4e08
jiangsyz <305460883@qq.com>
2018-05-21 10:48:46 +0800

    同意退款api

--- backend/controllers/RefundController.php
+++ backend/controllers/RefundController.php
@@ -93,14 +93,37 @@ class RefundController extends SmartWebController{
 			//获取购物行为
 			$buyingRecord=orderBuyingRecord::find()->where("`id`='{$buyingRecordId}'")->one();
 			if(!$buyingRecord) throw new SmartException("购物行为不存在",2);
-			//获取订单的所有购物行为
-			$buyingRecords=$orderRecord->buyingManagement->getBuyingList();
-			//判断退款的购物行为是否是该订单的
-			$flag=false;
-			foreach($buyingRecords as $v) if($v->id==$buyingRecord->id) $flag=true;
-			if(!$flag) throw new SmartException("退款购物行为不属于该订单",2);
 			//退款
 			$orderRecord->refundManagement->allpyRefundByBuyingRecord($staff,$buyingRecord,$price,$memo);
+			//提交事务
+			$trascation->commit();
+			//返回
+			$this->response(1,array('error'=>0));
+		}
+		catch(Exception $e){
+			//回滚
+			$trascation->rollback();
+			$this->response(1,array('error'=>$e->getCode()?$e->getCode():-1,'msg'=>$e->getMessage()));
+    	}
+	}
+	//========================================
+	//同意退款
+	public function actionApiRefund(){
+		try{
+			//开启事务
+			$trascation=Yii::$app->db->beginTransaction();
+			//根据token获取员工
+			$token=Yii::$app->request->get('token',false);
+			$staff=tokenManagement::getManagement($token,array(source::TYPE_STAFF))->getOwner();
+			//获取订单id
+			$orderId=Yii::$app->request->get('orderId',0);
+			//获取退款记录id
+			$refundId=Yii::$app->request->get('refundId',0);
+			//获取订单
+			$orderRecord=orderRecord::getLockedOrderById($orderId);
+			if(!$orderRecord) throw new SmartException("miss orderRecord");
+			//退款
+			$orderRecord->refundManagement->refund($staff,$refundId);
 			//提交事务
 			$trascation->commit();
 			//返回
