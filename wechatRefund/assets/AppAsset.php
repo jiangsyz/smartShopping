a2b80bf3fbe1e7bc3a0a0e8b5fc4f422c796a757
jiangsyz <305460883@qq.com>
2018-05-21 12:19:42 +0800

    init app wechatRefund

---
+++ wechatRefund/assets/AppAsset.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace wechatRefund\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main wechatRefund application asset bundle.
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/site.css',
+    ];
+    public $js = [
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
---
+++ wechatRefund/tests/functional/AboutCest.php
@@ -0,0 +1,13 @@
+<?php
+namespace wechatRefund\tests\functional;
+
+use wechatRefund\tests\FunctionalTester;
+
+class AboutCest
+{
+    public function checkAbout(FunctionalTester $I)
+    {
+        $I->amOnRoute('site/about');
+        $I->see('About', 'h1');
+    }
+}
