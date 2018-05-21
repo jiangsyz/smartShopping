4a964bfb67a233c827d1cb8a7e488c9cddfad04a
jiangsyz <305460883@qq.com>
2018-05-21 10:47:41 +0800

    重构orderRefundManagement

--- backend/models/order/orderRefundManagement.php
+++ backend/models/order/orderRefundManagement.php
@@ -11,12 +11,70 @@ use backend\models\pay\payCallback;
 class orderRefundManagement extends Component{
 	//订单记录
 	public $orderRecord=NULL;
+	//退款池
+	public $refunds=false;
 	//========================================
-	//获取该订单有效退款记录(剔除驳回的)
+	//初始化退款池
+	public function initRefunds(){
+		//只初始化一次
+		if($this->refunds!==false) return;
+		//加锁获取某个订单的所有退款记录
+		$this->refunds=refund::getRefundsOfOrder($this->orderRecord->id);;
+	}
+	//========================================
+	//获取退款池
+	public function getRefunds(){
+		$this->initRefunds();
+		return $this->refunds;
+	}
+	//========================================
+	//获取有效的退款(剔除驳回的)
 	public function getActiveRefunds(){
-		$tableName=refund::tableName();
-		$sql="SELECT * FROM {$tableName} WHERE `oid`='{$this->orderRecord->id}' AND `status`<>'-1' FOR UPDATE";
-		return refund::findBySql($sql)->all();
+		$refunds=array();
+		foreach($this->getRefunds() as $refund) 
+			if($refund->status!=refund::STATUS_REJECT) $refunds[]=$refund;
+		return $refunds;
+	}
+	//========================================
+	//按购买行为id作为线索获取有效的退款(剔除驳回的)
+	public function getActiveRefundsByBid(){
+		$refunds=array();
+		foreach($this->getActiveRefunds() as $refund) $refunds[$refund->bid][]=$refund;
+		return $refunds;
+	}
+	//========================================
+	//获取总退款金额
+	public function getTotalPrice(){
+		$totalPrice=0;
+		foreach($this->getActiveRefunds() as $refund) $totalPrice+=$refund->price;
+		return $totalPrice;
+	}
+	//========================================
+	//刷新
+	public function flushRefunds(){$this->refunds=false;}
+	//========================================
+	//检查
+	public function checkRefunds(){
+		//刷新
+		$this->flushRefunds();
+		//未支付订单不能涉及退款相关操作
+		if($this->orderRecord->payStatus!=1) throw new SmartException("未支付订单不能涉及退款相关操作",-2);
+		//退款总额不能超过订单支付总金额
+		if($this->getTotalPrice()>$this->orderRecord->pay) throw new SmartException("退款总额大于订单支付金额",-2);
+		//获取有效的退款
+		$activeRefunds=$this->getActiveRefundsByBid();
+		//校验互斥
+		foreach($activeRefunds as $bid => $v){
+			//每个bid只允许有一个有效退款
+			if(count($v)!=1) throw new SmartException("bid重复",-2);
+			//整单退款和单个购物行为退款互斥
+			if($bid!="0" && isset($refunds["0"])) throw new SmartException("bid互斥",-2);
+		}
+		//修改订单相关数据
+		if(!empty($activeRefunds)) 
+			$this->orderRecord->updateObj(array('refundingStatus'=>1,'finishStatus'=>0));
+		else
+			$this->orderRecord->updateObj(array('refundingStatus'=>0));
 	}
 	//========================================
 	//申请针对整单的退款
@@ -48,6 +106,9 @@ class orderRefundManagement extends Component{
 		$allowStatusList[]=orderStatusManagement::STATUS_UNRECEIPTED;
 		$allowStatusList[]=orderStatusManagement::STATUS_FINISHED;
 		if(!in_array($orderStatus,$allowStatusList)) throw new SmartException("订单状态错误",-2);
+		//判断购物行为是否属于订单
+		$isMyBuyingRecord=$this->orderRecord->buyingManagement->isMyBuyingRecord($orderBuyingRecord);
+		if(!$isMyBuyingRecord)throw new SmartException("购买行为不属于该订单",-2);
 		//必须要备注
 		if(!$memo) throw new SmartException("必须要备注",-2);
 		//添加退款记录
@@ -66,14 +127,11 @@ class orderRefundManagement extends Component{
 	//驳回
 	public function reject(source $handler,$refundId,$memo){
 		//获取退款记录
-		$tableName=refund::tableName();
-		$refund=refund::findBySql("SELECT * FROM {$tableName} WHERE `id`='{$refundId}' FOR UPDATE")->one();
-		//找不到
-		if(!$refund) throw new SmartException("找不到该退款记录",-2);
+		$refund=refund::getRefund($refundId); if(!$refund) throw new SmartException("找不到该退款记录",-2);
 		//订单不对
 		if($refund->oid!=$this->orderRecord->id) throw new SmartException("订单关系错误",-2);
 		//状态错误
-		if($refund->status!=0) throw new SmartException("退款记录状态错误",-2);
+		if($refund->status!=refund::STATUS_TODO) throw new SmartException("退款记录状态错误",-2);
 		//必须要备注
 		if(!$memo) throw new SmartException("必须要备注",-2);
 		//驳回
@@ -82,74 +140,48 @@ class orderRefundManagement extends Component{
 		$update['rejectHandlerId']=$handler->getSourceId();
 		$update['rejectTime']=time();
 		$update['rejectMemo']=$memo;
-		$update['status']=-1;
+		$update['status']=refund::STATUS_REJECT;
 		$refund->updateObj($update);
-		//如果驳回后订单没有进行中的退款,将订单的退款中标示位置为不在退款中
-		if(!$this->getActiveRefunds()) $this->orderRecord->updateObj(array('refundingStatus'=>0));
+		//检查订单的退款情况
+		$this->checkRefunds();
 	}
 	//========================================
 	//重开
 	public function reopen($refundId){
 		//获取退款记录
-		$tableName=refund::tableName();
-		$refund=refund::findBySql("SELECT * FROM {$tableName} WHERE `id`='{$refundId}' FOR UPDATE")->one();
-		//找不到
-		if(!$refund) throw new SmartException("找不到该退款记录",-2);
+		$refund=refund::getRefund($refundId); if(!$refund) throw new SmartException("找不到该退款记录",-2);
 		//订单不对
 		if($refund->oid!=$this->orderRecord->id) throw new SmartException("订单关系错误",-2);
 		//状态错误
-		if($refund->status!=-1) throw new SmartException("退款记录状态错误",-2);
+		if($refund->status!=refund::STATUS_REJECT) throw new SmartException("退款记录状态错误",-2);
 		//重开
 		$update=array();
 		$update['rejectHandlerType']=NULL;
 		$update['rejectHandlerId']=NULL;
 		$update['rejectTime']=NULL;
 		$update['rejectMemo']=NULL;
-		$update['status']=0;
+		$update['status']=refund::STATUS_TODO;
 		$refund->updateObj($update);
 		//检查订单的退款情况
 		$this->checkRefunds();
 	}
 	//========================================
-	//检查退款池
-	public function checkRefunds(){
-		//初始化退款总金额
-		$totalRefundPrice=0;
-		//初始化退款池
-		$refunds=array();
-		//未支付订单不能申请退款
-		if($this->orderRecord->payStatus!=1) throw new SmartException("未支付订单不能申请退款",-2);
-		//获取该订单有效退款记录
-		$activeRefunds=$this->getActiveRefunds();
-		//统计退款总金额并以购物行为id为key填充退款池
-		foreach($activeRefunds as $v){
-			$totalRefundPrice+=$v->price;
-			$refunds[$v->bid][]=$v;
-		}
-		//退款总额不能超过订单总价
-		if($totalRefundPrice>$this->orderRecord->pay) throw new SmartException("退款总额大于订单支付金额",-2);
-		//校验互斥
-		foreach($refunds as $bid=>$v){
-			//每个bid只允许有一个有效退款
-			if(count($v)!=1) throw new SmartException("bid重复",-2);
-			//整单退款和单个购物行为退款互斥
-			if($bid!="0" && isset($refunds["0"])) throw new SmartException("bid互斥",-2);
-		}
-		//修改订单相关数据
-		if(!empty($refunds)) $this->orderRecord->updateObj(array('refundingStatus'=>1,'finishStatus'=>0));
-	}
-	//========================================
 	//退款
-	public function refund($refundId){
+	public function refund(source $handler,$refundId){
 		//获取退款记录
-		$tableName=refund::tableName();
-		$refund=refund::findBySql("SELECT * FROM {$tableName} WHERE `id`='{$refundId}' FOR UPDATE")->one();
-		//找不到
-		if(!$refund) throw new SmartException("找不到该退款记录",-2);
+		$refund=refund::getRefund($refundId); if(!$refund) throw new SmartException("找不到该退款记录",-2);
 		//订单不对
 		if($refund->oid!=$this->orderRecord->id) throw new SmartException("订单关系错误",-2);
 		//状态错误
-		if($refund->status!=0) throw new SmartException("退款记录状态错误",-2);
+		if($refund->status!=refund::STATUS_TODO) throw new SmartException("退款记录状态错误",-2);
+		//退款
+		$update=array();
+		$update['refundHandlerType']=$handler->getSourceType();
+		$update['refundHandlerId']=$handler->getSourceId();
+		$update['refundTime']=time();
+		$update['refundMemo']=Yii::$app->controller->runningId;;
+		$update['status']=refund::STATUS_REFUNDING;
+		$refund->updateObj($update);
 		//检查退款池
 		$this->checkRefunds();
 		//查找支付回调
