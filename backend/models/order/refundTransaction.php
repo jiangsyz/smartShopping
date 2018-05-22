bdfe75ed98f3bb1209737cb3946beb3509b84865
jiangsyz <305460883@qq.com>
2018-05-22 18:17:57 +0800

    refundTransaction增加获取退款/交易成功/交易失败方法

--- backend/models/order/refundTransaction.php
+++ backend/models/order/refundTransaction.php
@@ -18,4 +18,35 @@ class refundTransaction extends SmartActiveRecord{
 	//========================================
 	//初始化交易状态
 	public function initStatus(){$this->status=0;}
+	//========================================
+	//获取退款
+	public function getRefund(){
+		$table=refund::tableName();
+		$sql="SELECT * FROM {$table} WHERE `id`='{$this->refundId}' FOR UPDATE;";
+		return refund::findBySql($sql)->one();
+	}
+	//========================================
+	//交易成功
+	public function transactionSuccess(){
+		//交易状态必须是打款中才能切换至成功
+		if($this->status!=0) throw new SmartException("error transaction status");
+		//获取退款
+		$refund=$this->getRefund(); if(!$refund) throw new SmartException("miss refund");
+		//将交易状态切换为成功
+		$this->updateObj(array('status'=>1));
+		//将退款状态切为打款成功
+		$refund->transactionSuccess();
+	}
+	//========================================
+	//交易失败
+	public function transactionFail(){
+		//交易状态必须是打款中才能切换至失败
+		if($this->status!=0) throw new SmartException("error transaction status");
+		//获取退款
+		$refund=$this->getRefund(); if(!$refund) throw new SmartException("miss refund");
+		//将交易状态切换为成功
+		$this->updateObj(array('status'=>-1));
+		//将退款状态切为打款失败
+		$refund->transactionFail();
+	}
 }
