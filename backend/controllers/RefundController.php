90c363e25adb6afa5fb1b98721f8af5f0fbf09ff
jiangsyz <305460883@qq.com>
2018-05-22 18:59:34 +0800

    退款重置api

--- backend/controllers/RefundController.php
+++ backend/controllers/RefundController.php
@@ -135,4 +135,33 @@ class RefundController extends SmartWebController{
 			$this->response(1,array('error'=>$e->getCode()?$e->getCode():-1,'msg'=>$e->getMessage()));
     	}
 	}
+	//========================================
+	//重置退款
+	public function actionApiReset(){
+		try{
+			//开启事务
+			$trascation=Yii::$app->db->beginTransaction();
+			//根据token获取员工
+			$token=Yii::$app->request->get('token',false);
+			$staff=tokenManagement::getManagement($token,array(source::TYPE_STAFF))->getOwner();
+			//获取订单id
+			$orderId=Yii::$app->request->get('orderId',0);
+			//获取退款记录id
+			$refundId=Yii::$app->request->get('refundId',0);
+			//获取订单
+			$orderRecord=orderRecord::getLockedOrderById($orderId);
+			if(!$orderRecord) throw new SmartException("miss orderRecord");
+			//重开退款
+			$orderRecord->refundManagement->reset($refundId);
+			//提交事务
+			$trascation->commit();
+			//返回
+			$this->response(1,array('error'=>0));
+		}
+		catch(Exception $e){
+			//回滚
+			$trascation->rollback();
+			$this->response(1,array('error'=>$e->getCode()?$e->getCode():-1,'msg'=>$e->getMessage()));
+    	}
+	}
 }
