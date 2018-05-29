047d943b0d7403d1d9ca08ed22cb9bba98208a55
jiangsyz <305460883@qq.com>
2018-05-29 11:59:35 +0800

    WechatController打日志/测试方法

--- backend/controllers/WechatController.php
+++ backend/controllers/WechatController.php
@@ -27,6 +27,8 @@ class WechatController extends SmartWebController{
             $openid=$data['openid'];
             //解密用户加密信息
             $data=Yii::$app->smartWechat->encrypteData($sessionKey,$iv,$encryptedData);
+            //日志
+            Yii::$app->smartLog->debugLog(json_encode($data));
             //提取手机号
             if(!isset($data['purePhoneNumber'])) throw new SmartException("miss purePhoneNumber");
             $phone=$data['purePhoneNumber'];
@@ -48,5 +50,14 @@ class WechatController extends SmartWebController{
             $trascation->rollback();
             $this->response(1,array('error'=>$e->getCode()?$e->getCode():-1,'msg'=>$e->getMessage()));
         }
+    }
+    //========================================
+    public function actionIndex(){
+        $encryptedData="5D6p5giaozmyFyh2AtPGuGxM0Vj9deY5V2LG+xN6wppy870N+dtP2o+QQ9BY\/Fhx0rT+Ogg2f8elCIfdQ5b0CCbU107lfcQ0sG2cza7nO2WeLQi6o0JwoReUw0llBQ68HbhiLS3ZB1Y6aE8KiRnqgQiYtcNhQE2Yq\/96QMy6w0E19u\/1tXDlSJa725RReLzw\/TyMZJagIqApDzZTaVMOIA==";
+        $iv="jZkcHgGUEQIS7fnp9BlYFQ==";
+        $sessionKey="gAIbqA642JtOJZJuNm1nMQ==";
+        $data=Yii::$app->smartWechat->encrypteData($sessionKey,$iv,$encryptedData);
+
+        var_dump($data);
     }
 }
