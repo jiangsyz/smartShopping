66cae4337fd895268d3118fdbd977f15691ba099
jiangsyz <305460883@qq.com>
2018-06-01 15:41:13 +0800

    add logistics.php

---
+++ backend/models/product/logistics.php
@@ -0,0 +1,8 @@
+<?php
+//物流
+namespace backend\models\product;
+use Yii;
+use yii\base\SmartException;
+use yii\db\SmartActiveRecord;
+//========================================
+class logistics extends SmartActiveRecord{}
