394d1ff5c0cb7f1895ed2ef5205da8e0fe6d34d8
jiangsyz <305460883@qq.com>
2018-06-05 15:45:21 +0800

    actionActionTracker优化

--- console/controllers/LogController.php
+++ console/controllers/LogController.php
@@ -2,28 +2,36 @@
 namespace console\controllers;
 use Yii;
 use yii\console\Controller;
+use yii\base\SmartException;
+use yii\base\Exception;
 use backend\models\actionTracker\actionTracker;
 class LogController extends Controller{
 	//用户行为追踪
     public function actionActionTracker(){
+    	//记录日志
+    	Yii::$app->smartLog->consoleLog('start running');
+    	//循环处理
     	while(1){
     		try{
 				//开启事务
 				$trascation=Yii::$app->db->beginTransaction();
 				//找到跟踪过的最后一条日志
 				$lastOne=actionTracker::find()->orderBy("`logId` DESC")->one();
-				//从下一条起继续跟踪
+				//确定跟踪日志的起始id
 				$startId=$lastOne?$lastOne->logId+1:1;
+				//记录日志
+				Yii::$app->smartLog->consoleLog('log startId='.$startId);
+				//从起始id开始进行行为跟踪
 				actionTracker::actionTrack($startId);
 				//提交事务
 				$trascation->commit();
-				//休息一下
-				sleep(5);
     		}
 	    	catch(Exception $e){
 	    		//回滚
 				$trascation->rollback();
 	    	}
+	    	//休息一下
+			sleep(Yii::$app->params["consoleSleep"]);
     	}
     }
 }
