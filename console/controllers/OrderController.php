1527273ca64d25c4a72b377d96ff2e1bb9b0cb5a
jiangsyz <305460883@qq.com>
2018-06-07 14:25:02 +0800

    订单支付超时检测/订单支付超时或取消或关闭返库存 将try颗粒度划到单个订单

--- console/controllers/OrderController.php
+++ console/controllers/OrderController.php
@@ -20,37 +20,37 @@ class OrderController extends SmartDaemonController{
     	$this->begin();
     	//循环处理
     	while(1){
-    		try{
-				//开启事务
-				$trascation=Yii::$app->db->beginTransaction();
-				//获取未支付的订单id
-				$table=orderRecord::tableName();
-				$sql=
-				"
-				SELECT `id` FROM {$table} 
-				WHERE 
-					`parentId` is NULL 
-					AND 
-					`payStatus`='0' 
-					AND 
-					`cancelStatus`='0' 
-					AND 
-					`closeStatus`='0';
-				";
-				$rows=Yii::$app->db->createCommand($sql)->queryAll();
-				//循环检查超时
-				foreach($rows as $row){
+    		//获取未支付的订单id
+			$table=orderRecord::tableName();
+			$sql=
+			"
+			SELECT `id` FROM {$table} 
+			WHERE 
+				`parentId` is NULL 
+				AND 
+				`payStatus`='0' 
+				AND 
+				`cancelStatus`='0' 
+				AND 
+				`closeStatus`='0';
+			";
+			$rows=Yii::$app->db->createCommand($sql)->queryAll();
+			//循环检查超时
+			foreach($rows as $row){
+				try{
+					//开启事务
+					$trascation=Yii::$app->db->beginTransaction();
 					//获取订单
 					$orderRecord=orderRecord::getLockedOrderById($row['id']);
 					//检查超时
 					$orderRecord->payManagement->canOrderPay();
 					//记录日志
 					Yii::$app->smartLog->consoleLog("check order {$orderRecord->id}");
+					//提交事务
+					$trascation->commit();
 				}
-				//提交事务
-				$trascation->commit();
-    		}
-	    	catch(Exception $e){$trascation->rollback();}
+				catch(Exception $e){$trascation->rollback();}
+			}
 	    	//休息一下
 			$this->sleep();
 			//报告存活
@@ -63,35 +63,35 @@ class OrderController extends SmartDaemonController{
 		$this->begin();
     	//循环处理
     	while(1){
-    		try{
-				//开启事务
-				$trascation=Yii::$app->db->beginTransaction();
-				//获取支付超时或取消或关闭的订单id
-				$table=orderRecord::tableName();
-				$sql=
-				"
-				SELECT `id` FROM {$table} 
-				WHERE 
-					`parentId` is NULL 
-					AND 
-					(`payStatus`='-1' OR `cancelStatus`='1' OR `closeStatus`='1') 
-					AND 
-					`backKeepCountStatus`='0';
-				";
-				$rows=Yii::$app->db->createCommand($sql)->queryAll();
-				//循环检返库存
-				foreach($rows as $row){
+    		//获取支付超时或取消或关闭的订单id
+			$table=orderRecord::tableName();
+			$sql=
+			"
+			SELECT `id` FROM {$table} 
+			WHERE 
+				`parentId` is NULL 
+				AND 
+				(`payStatus`='-1' OR `cancelStatus`='1' OR `closeStatus`='1') 
+				AND 
+				`backKeepCountStatus`='0';
+			";
+			$rows=Yii::$app->db->createCommand($sql)->queryAll();
+			//循环检返库存
+			foreach($rows as $row){
+				try{
+					//开启事务
+					$trascation=Yii::$app->db->beginTransaction();
 					//加锁取订单
 					$orderRecord=orderRecord::getLockedOrderById($row['id']);
 					//返库存
 					$orderRecord->cancelManagement->backKeepCount();
 					//记录日志
 					Yii::$app->smartLog->consoleLog("check order {$orderRecord->id}");
+					//提交事务
+					$trascation->commit();
 				}
-				//提交事务
-				$trascation->commit();
+				catch(Exception $e){$trascation->rollback();}
 			}
-			catch(Exception $e){$trascation->rollback();}	
 	    	//休息一下
 			$this->sleep();
 			//报告存活
