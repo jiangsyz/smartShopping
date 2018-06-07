3b6091e2f48f8d1fb674b5462aba7b2a3528a912
jiangsyz <305460883@qq.com>
2018-06-07 11:52:42 +0800

    命令行actionBackKeepCount

--- console/controllers/OrderController.php
+++ console/controllers/OrderController.php
@@ -16,7 +16,7 @@ use backend\models\notice\notice;
 use backend\models\order\orderRecord;
 class OrderController extends SmartDaemonController{
 	//订单支付超时检测
-    public function actionCheckOrderPayTimeout(){
+    public function actionCheckPayTimeout(){
     	$this->begin();
     	//循环处理
     	while(1){
@@ -27,16 +27,15 @@ class OrderController extends SmartDaemonController{
 				$table=orderRecord::tableName();
 				$sql=
 				"
-						SELECT `id` FROM {$table} 
-						WHERE 
-							`parentId` is NULL 
-							AND 
-							`payStatus`='0' 
-							AND 
-							`cancelStatus`='0' 
-							AND 
-							`closeStatus`='0' 
-						ORDER BY `createTime`;
+				SELECT `id` FROM {$table} 
+				WHERE 
+					`parentId` is NULL 
+					AND 
+					`payStatus`='0' 
+					AND 
+					`cancelStatus`='0' 
+					AND 
+					`closeStatus`='0';
 				";
 				$rows=Yii::$app->db->createCommand($sql)->queryAll();
 				//循环检查超时
@@ -51,10 +50,7 @@ class OrderController extends SmartDaemonController{
 				//提交事务
 				$trascation->commit();
     		}
-	    	catch(Exception $e){
-	    		//回滚
-				$trascation->rollback();
-	    	}
+	    	catch(Exception $e){$trascation->rollback();}
 	    	//休息一下
 			$this->sleep();
 			//报告存活
@@ -62,5 +58,46 @@ class OrderController extends SmartDaemonController{
     	}
     }
     //========================================
+	//订单支付超时或取消或关闭返库存
+	public function actionBackKeepCount(){
+		$this->begin();
+    	//循环处理
+    	while(1){
+    		try{
+				//开启事务
+				$trascation=Yii::$app->db->beginTransaction();
+				//获取支付超时或取消或关闭的订单id
+				$table=orderRecord::tableName();
+				$sql=
+				"
+				SELECT `id` FROM {$table} 
+				WHERE 
+					`parentId` is NULL 
+					AND 
+					(`payStatus`='-1' OR `cancelStatus`='1' OR `closeStatus`='1') 
+					AND 
+					`backKeepCountStatus`='0';
+				";
+				$rows=Yii::$app->db->createCommand($sql)->queryAll();
+				//循环检返库存
+				foreach($rows as $row){
+					//加锁取订单
+					$orderRecord=orderRecord::getLockedOrderById($row['id']);
+					//返库存
+					$orderRecord->cancelManagement->backKeepCount();
+					//记录日志
+					Yii::$app->smartLog->consoleLog("check order {$orderRecord->id}");
+				}
+				//提交事务
+				$trascation->commit();
+			}
+			catch(Exception $e){$trascation->rollback();}	
+	    	//休息一下
+			$this->sleep();
+			//报告存活
+			$this->alive();
+    	}
+	}
+	//========================================
 }
 ?>
