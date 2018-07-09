01744e8a838793c724439fad5417800aef422349
jiangsyz <305460883@qq.com>
2018-07-09 15:29:44 +0800

    models/member/youzanCard.php

---
+++ backend/models/member/youzanCard.php
@@ -0,0 +1,8 @@
+<?php
+//用户有赞vip
+namespace backend\models\member;
+use Yii;
+use yii\base\SmartException;
+use yii\db\SmartActiveRecord;
+//========================================
+class youzanCard extends SmartActiveRecord{}
