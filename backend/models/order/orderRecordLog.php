27d01e28d96889e5aa383eeebcdfb0503cf4c7ab
jiangsyz <305460883@qq.com>
2018-07-17 17:21:17 +0800

    增加orderRecordLog表

---
+++ backend/models/order/orderRecordLog.php
@@ -0,0 +1,22 @@
+<?php
+//订单记录
+namespace backend\models\order;
+use Yii;
+use yii\base\SmartException;
+use yii\db\SmartActiveRecord;
+//========================================
+class orderRecordLog extends SmartActiveRecord{
+	//初始化
+	public function init(){
+		parent::init();
+		$this->on(self::EVENT_BEFORE_INSERT,array($this,"initTime"));
+		$this->on(self::EVENT_BEFORE_INSERT,array($this,"initRunningId"));
+	}
+	//========================================
+	public function initTime(){$this->time=time();}
+	//========================================
+	public function initRunningId(){
+		if(!Yii::$app->controller->runningId)throw new SmartException("miss runningId");
+		$this->runningId=Yii::$app->controller->runningId;
+	}
+}
