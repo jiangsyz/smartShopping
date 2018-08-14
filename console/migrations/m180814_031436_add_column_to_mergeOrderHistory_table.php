73d73e94286eea488aeef18bd14872f675873f0a
jiangsyz <305460883@qq.com>
2018-08-14 15:51:59 +0800

    mergeOrderHistory修改mergeId字段sql

--- console/migrations/m180814_031436_add_column_to_mergeOrderHistory_table.php
+++ console/migrations/m180814_031436_add_column_to_mergeOrderHistory_table.php
@@ -2,6 +2,6 @@
 use yii\db\Migration;
 class m180814_031436_add_column_to_mergeOrderHistory_table extends Migration{
     public function up(){
-        $this->addColumn('merge_order_history','mergeId','INT(10) DEFAULT NULL COMMENT "合单号" AFTER `id`');
+        $this->addColumn('merge_order_history','mergeId','VARCHAR(200) DEFAULT NULL COMMENT "合单号" AFTER `id`');
     }
 }
