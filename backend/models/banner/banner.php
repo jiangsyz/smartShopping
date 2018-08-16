f2debf0e1a99deeb212662c32459af0e1f76e30a
wuzhiyibq <282414351@qq.com>
2018-08-16 17:28:15 +0800

    banner图只显示3张

--- backend/models/banner/banner.php
+++ backend/models/banner/banner.php
@@ -14,6 +14,10 @@ class banner extends LogActiveRecord{
 		$banners=banner::find()->where("`siteNo`='{$siteNo}'")->orderBy("`sort` ASC")->all();
 		//组织数据
 		foreach($banners as $b) {
+			//只展示3张banner图
+			if (count($data) >= 3) {
+				break;
+			}
 			$tmp = explode(',', $b->uri);
 			//查看SPU的库存是否为0  是否上架
 			$spu = spu::find()->where("id={$tmp[1]}")->one();
